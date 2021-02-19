5e047e30f8e05b84de4ecb72162f1f5c3a21a634
Nova Wittam <nova@wittamit.co.uk>
2021-02-19 19:45:51 +0000

    Created file skeletons for mapManager class

---
+++ src/mapManager.cpp
@@ -0,0 +1,6 @@
+//
+// Created by nova on 19/02/2021.
+//
+
+#include "mapManager.hpp"
+
---
+++ src/mapManager.hpp
@@ -0,0 +1,11 @@
+//
+// Created by nova on 19/02/2021.
+//
+
+#ifndef INC_2DGAME_MAPMANAGER_HPP
+#define INC_2DGAME_MAPMANAGER_HPP
+
+class mapManager {
+};
+
+#endif//INC_2DGAME_MAPMANAGER_HPP
