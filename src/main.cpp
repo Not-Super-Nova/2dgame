05d121ed8d84d5b736cbcbefc067ae0e99038327
Nova Wittam <nova@wittamit.co.uk>
2021-02-19 18:26:15 +0000

    Made new keyboard detection system work, and shuffled around some functions and data. Also converted to CMake, to make building easier.

--- src/main.cpp
+++ src/main.cpp
@@ -3,22 +3,57 @@
 #include <SDL2/SDL_events.h>
 #include <SDL2/SDL_hints.h>
 #include <SDL2/SDL_image.h>
+#include <SDL2/SDL_keyboard.h>
 #include <SDL2/SDL_keycode.h>
+#include <SDL2/SDL_render.h>
+#include <SDL2/SDL_stdinc.h>
 #include <SDL2/SDL_surface.h>
 #include <SDL2/SDL_video.h>
-#include <stdio.h>
-#include <string.h>
+#include <cstdio>
+#include <cstring>
 
-#include "constants.h"
+#include "main.h"
+
+const int gcScreenWidth = 640;
+const int gcScreenHeight = 480;
+const char *gcWindowTitle = "Simple 2D Game";
+SDL_Window *gWindow = NULL;
+SDL_Renderer *gRenderer = NULL;
+Uint8 *gKeyboardState;
+int gMovementSpeed = 2;
+SDL_Surface *gScreenSurface = NULL;
+SDL_Surface *gKeyPressSurfaces[KEY_PRESS_SURFACE_TOTAL];
+SDL_Surface *gCurrentSurface = NULL;
+SDL_Texture *gCharacterImage = NULL;
 
 SDL_Surface *loadSurface(char *path) {
   // Load image at specified path
   SDL_Surface *loadedSurface = IMG_Load(path);
   if (loadedSurface == NULL) {
     printf("Unable to load image %s! SDL Error: %s\n", path, SDL_GetError());
+    return NULL;
+  } else {
+    // SDL_Surface *optimizedSurface =
+    //    SDL_ConvertSurface(loadedSurface, gScreenSurface->format, 0);
+    // if (optimizedSurface == NULL) {
+    //  printf("Failed to optimise %s: %s", path, SDL_GetError());
+    return loadedSurface;
+    //} else {
+    //  return optimizedSurface;
   }
+}
 
-  return loadedSurface;
+// return loadedSurface;
+//}
+
+SDL_Texture *loadTexture(char *path) {
+  SDL_Texture *loadedTexture = IMG_LoadTexture(gRenderer, path);
+  if (loadedTexture == NULL) {
+    printf("Unable to load image %s! SDL Error: %s\n", path, SDL_GetError());
+    return NULL;
+  } else {
+    return loadedTexture;
+  }
 }
 
 bool init() {
@@ -33,15 +68,19 @@ bool init() {
     gWindow =
         SDL_CreateWindow(gcWindowTitle, SDL_WINDOWPOS_UNDEFINED,
                          SDL_WINDOWPOS_UNDEFINED, gcScreenWidth, gcScreenHeight,
-                         SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN);
+                         SDL_WINDOW_SHOWN /*| SDL_WINDOW_FULLSCREEN*/);
     if (gWindow == NULL) {
       printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
       success = false;
     } else {
-
+      gRenderer = SDL_CreateRenderer(
+          gWindow, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
       gScreenSurface = SDL_GetWindowSurface(gWindow);
     }
   }
+
+  gKeyboardState = (Uint8 *)SDL_GetKeyboardState(NULL);
+
   return success;
 }
 
@@ -77,13 +116,22 @@ bool loadMedia() {
     success = false;
   }
 
+  gCharacterImage = loadTexture((char *)"media/character.png");
+
   return success;
 }
 
 void close() {
 
-  SDL_FreeSurface(gTestImage);
-  gTestImage = NULL;
+  for (int i = 0; i < KEY_PRESS_SURFACE_TOTAL; i++) {
+    SDL_FreeSurface(gKeyPressSurfaces[i]);
+  }
+
+  SDL_DestroyTexture(gCharacterImage);
+  gCharacterImage = NULL;
+
+  SDL_DestroyRenderer(gRenderer);
+  gRenderer = NULL;
 
   SDL_DestroyWindow(gWindow);
   gWindow = NULL;
@@ -98,48 +146,50 @@ int main(int argc, char *args[]) {
     if (!loadMedia()) {
       printf("Failed to load media!\n");
     } else {
-      SDL_BlitSurface(gTestImage, NULL, gScreenSurface, NULL);
-      SDL_UpdateWindowSurface(gWindow);
-
       bool quit = false;
       SDL_Event e;
       gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
+      Sprite *playerCharacter =
+          new Sprite(gCharacterImage, 0.0f, 0.0f, 128, 128, 0);
+      playerCharacter->currentMap = 0;
+      playerCharacter->texture = gCharacterImage;
+      int width, height;
+      SDL_QueryTexture(playerCharacter->texture, NULL, NULL, &width, &height);
+      playerCharacter->height = width;
+      playerCharacter->width = height;
+      playerCharacter->rect->w = width;
+      playerCharacter->rect->h = height;
+      playerCharacter->locationX = 0.0f;
+      playerCharacter->locationY = 0.0f;
 
       while (!quit) {
-        while (SDL_PollEvent(&e) != 0) {
-          switch (e.type) {
-          case SDL_QUIT:
-            printf("Quit requested, shutting down");
-            quit = true;
-            break;
-          case SDL_KEYDOWN:
-            switch (e.key.keysym.sym) {
-            case SDLK_w:
-              gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_W];
-              break;
-            case SDLK_s:
-              gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_S];
-              break;
-            case SDLK_a:
-              gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_A];
-              break;
-            case SDLK_d:
-              gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_D];
-              break;
-            default:
-              gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
-              break;
-            }
-            break;
-
-          case SDL_KEYUP:
-            gCurrentSurface = gKeyPressSurfaces[KEY_PRESS_SURFACE_DEFAULT];
-            break;
-          }
+        if (gKeyboardState[SDL_SCANCODE_ESCAPE]) {
+          printf("Quit requested, shutting down");
+          quit = true;
         }
-        SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
+        if (gKeyboardState[SDL_SCANCODE_W]) {
+          playerCharacter->locationY -= gMovementSpeed;
+        }
+        if (gKeyboardState[SDL_SCANCODE_S]) {
+          playerCharacter->locationY += gMovementSpeed;
+        }
+        if (gKeyboardState[SDL_SCANCODE_A]) {
+          playerCharacter->locationX -= gMovementSpeed;
+        }
+        if (gKeyboardState[SDL_SCANCODE_D]) {
+          playerCharacter->locationX += gMovementSpeed;
+        }
+        SDL_RenderClear(gRenderer);
+        playerCharacter->rect->x = playerCharacter->locationX;
+        playerCharacter->rect->y = playerCharacter->locationY;
+        SDL_RenderCopy(gRenderer, playerCharacter->texture, NULL,
+                       playerCharacter->rect);
+        SDL_RenderPresent(gRenderer);
+        SDL_RenderFillRect(gRenderer, playerCharacter->rect);
+        SDL_PumpEvents();
+        // SDL_BlitSurface(gCurrentSurface, NULL, gScreenSurface, NULL);
 
-        SDL_UpdateWindowSurface(gWindow);
+        // SDL_UpdateWindowSurface(gWindow);
       }
     }
   }
--- testmedia.sh
+++ testmedia.sh
@@ -5,5 +5,7 @@ do
    inkscape -w 128 -h 128 -b "#ffffffff" image.svg -o "media/${keys:$i:1}.png";
 done;
 pango-view --font="sans mono" -qo image.svg --text "None";
-   inkscape -w 128 -h 128 -b "#ffffffff" image.svg -o "media/Default.png";
+inkscape -w 128 -h 128 -b "#ffffffff" image.svg -o "media/Default.png";
+pango-view --font="sans mono" -qo image.svg --text "Character";
+inkscape -w 128 -h 128 -b "#ffffffff" image.svg -o "media/character.png";
 rm image.svg
